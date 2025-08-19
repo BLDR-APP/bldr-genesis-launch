c1eef42fe3eca47430521918f0e98adeb5135525
BLDR-APP <use.bldr@gmail.com>
2025-08-18 23:03:01 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
       name: "Pedro Menin",
       role: "CEO & Fundador",
       description: "Visionário por trás do app, lidera a estratégia e a construção do futuro da plataforma fitness, conectando inovação e comunidade.",
-      image: "src/assets/images/Eu.png"
+      image: "https://imgur.com/IosFTGs"
     },
     {
       name: "Pedro Lacerda",
