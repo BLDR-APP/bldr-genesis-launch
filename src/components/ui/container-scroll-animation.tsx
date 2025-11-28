619da173907144588f4984e240559a792fcf8766
BLDR-APP <use.bldr@gmail.com>
2025-11-28 17:48:05 -0300

    Update container dimensions and add UI elements

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -84,9 +84,23 @@ export const Card = ({
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 bg-[#222222] rounded-[30px] shadow-2xl"
+      // MUDANÇAS AQUI: Dimensões de iPhone e bordas
+      className="max-w-[320px] md:max-w-[400px] -mt-12 mx-auto h-[650px] md:h-[850px] w-full border-[8px] border-[#4a4a4a] p-[6px] bg-[#222222] rounded-[55px] shadow-2xl relative"
     >
-      <div className=" h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
+      {/* Dynamic Island e Speaker */}
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[120px] h-[35px] bg-black rounded-b-[20px] z-50 flex items-center justify-center">
+         <div className="w-[80px] h-[6px] bg-zinc-800 rounded-full mr-2"></div>
+         <div className="w-[6px] h-[6px] bg-blue-900/50 rounded-full"></div>
+      </div>
+
+      {/* Botões Laterais (Volume/Power) - Detalhe Extra */}
+      <div className="absolute top-[100px] -left-[10px] w-[3px] h-[26px] bg-[#3a3a3a] rounded-l-md"></div>
+      <div className="absolute top-[140px] -left-[10px] w-[3px] h-[50px] bg-[#3a3a3a] rounded-l-md"></div>
+      <div className="absolute top-[200px] -left-[10px] w-[3px] h-[50px] bg-[#3a3a3a] rounded-l-md"></div>
+      <div className="absolute top-[150px] -right-[10px] w-[3px] h-[80px] bg-[#3a3a3a] rounded-r-md"></div>
+
+      {/* A Tela em si */}
+      <div className="h-full w-full overflow-hidden rounded-[45px] bg-black dark:bg-zinc-900">
         {children}
       </div>
     </motion.div>
